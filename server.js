517a94a6a089dbf334f84524507041a60e4a5020
Vaibhav Sidana <vaibhavsidana@Vaibhavs-MacBook-Pro.local>
2025-06-07 21:43:19 +0530

    additions

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const OpenAI = require("openai");
 const app = express();
 const prisma = new PrismaClient();
 const { differenceInHours } = require('date-fns');
+const axios = require('axios');
 
 
 app.use(cors());
-app.use(express.json()); // Middleware to parse JSON data
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 
 const openai = new OpenAI({
